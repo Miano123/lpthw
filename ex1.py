90ccd2ee715f3dbee4a219cee4d65950b4ab6d98
Miano123 <rachelmiano91@gmail.com>
2022-10-17 21:55:47 +0300

    Hello World!

---
+++ ex1.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hellow World!")
+    print("Hellow Again")
+    print('Yay! Printing.')
+    print("I'd much rather you 'not'.")
+    print('I "said" do not touch this.')
+main()
