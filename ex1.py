fa6fdadb998976bf3e558c05f4f8b5242558750c
Miano Rochelle <rachelmiano91@gmail.com>
2022-10-18 15:55:00 +0300

    Delete ex1.py

--- ex1.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("Hellow World!")
-    print("Hellow Again")
-    print('Yay! Printing.')
-    print("I'd much rather you 'not'.")
-    print('I "said" do not touch this.')
-main()
